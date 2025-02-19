b2cef858d039bd08f5ead4fda5357d51ba49b399
sarali-404 <saralithiloka@gmail.com>
2025-02-19 09:50:41 +0530

    settings damma

--- pages/setting.php
+++ pages/setting.php
@@ -7,7 +7,7 @@
     <!-- Add Bootstrap Icons CDN link -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/style.css"> <!-- Custom Styles -->
-    <link rel="stylesheet" href="../assets/css/module3.css"> <!-- Custom Styles -->
+    <link rel="stylesheet" href="../assets/css/setting.css"> <!-- Custom Styles -->
 
 
     
@@ -19,7 +19,60 @@
         include '../includes/navbar.php';  //meee wageeeee karannnn
         include '../includes/sidebar.php'; //meee wageeeee karannnn
     ?>
-    
+    <div class="settings-container">
+        <h2>Settings</h2>
+        
+        <div class="section">
+        <div class="section1">
+            <h3>Change Email</h3>
+            <form>
+                <div class="form-group">
+                    <label>Current email</label>
+                    <input type="email" value="sarialbalasinghe@gmail.com" disabled>
+                </div>
+                <div class="form-group">
+                    <label>Current password</label>
+                    <input type="password" placeholder="Enter current password">
+                </div>
+                <div class="form-group">
+                    <label>New email</label>
+                    <input type="email" placeholder="Enter new email">
+                </div>
+                <div class="form-group">
+                    <label>Repeat new email</label>
+                    <input type="email" placeholder="Enter new email again">
+                </div>
+                <button class="save-btn">Save changes</button>
+            </form>
+        </div>
+        
+        
+        <div class="section2">
+            <h3>Change password</h3>
+            <form>
+                <div class="form-group">
+                    <label>Current password</label>
+                    <input type="password" placeholder="Enter current password">
+                </div>
+                <div class="form-group">
+                    <label>New password</label>
+                    <input type="password" placeholder="Enter new password">
+                </div>
+                <div class="form-group">
+                    <label>Repeat new password</label>
+                    <input type="password" placeholder="Enter new password again">
+                </div>
+                <button class="save-btn">Save changes</button>
+            </form>
+        </div>
+        
+        <div class="section3">
+            <h3>Delete account</h3>
+            <p>Do you want to delete your ‘The Kuppiya’ account? We wish you all the best and hope to see you again soon</p>
+            <button class="delete-btn">Permanently delete account</button>
+        </div>
+        </div>
+    </div>
     
 
     <script src="../assets/js/script.js"></script>
