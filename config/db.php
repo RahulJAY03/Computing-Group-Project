fe920e210516c287ca83b94549633ab679d58f60
sarali-404 <saralithiloka@gmail.com>
2025-03-17 14:37:10 +0530

    tama hadana gmn-gulugulu auth

--- config/db.php
+++ config/db.php
@@ -1,4 +1,11 @@
 <?php
+header("Cross-Origin-Opener-Policy: same-origin-allow-popups");
+header("Cross-Origin-Embedder-Policy: require-corp");
+header("Access-Control-Allow-Origin: *"); // Or specify allowed domains
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 require_once __DIR__ . '/../vendor/autoload.php';
 // If vendor is in the parent directory
 ; // Ensure MongoDB driver is loaded
